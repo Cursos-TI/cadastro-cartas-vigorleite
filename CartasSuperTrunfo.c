b446535164520676ae264fce0a0ebc37578ca034
Igor Leite <igor.mr790@gmail.com>
2025-10-02 02:40:40 +0000

    FINALIZAÇÃO DA CRIAÇÃO DO DESAFIO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,43 +21,43 @@ int main() {
   // Será usado o printf para mostrar o texto e o scanf para ler o usuário
   // Dados da Carta 1
   printf ("Informe os dados da Carta 1\nEscolha de A a H para o nome do estado: ");
-  scanf (" %c", estado1);
-  printf ("Escolha entre A01,A04,A03,A04 para o código do estado: ");
+  scanf (" %c", &estado1);
+  printf ("Escolha entre 01,04,03,04 para o codigo do estado: ");
   scanf ("%s", codigo1);
   printf ("Digite o nome da cidade: ");
   scanf ("%s", cidade1);
   printf ("Digite a populacao dessa cidade: ");
-  scanf ("%d", populacao1);
+  scanf ("%d", &populacao1);
   printf ("Digite o tamanho em km² dessa cidade: ");
-  scanf ("%f", area1);
+  scanf ("%f", &area1);
   printf ("Digite o PIB dessa cidade: ");
-  scanf ("%f", pib1);
-  rintf ("Digite a quantidade de pontos turisticos dessa cidade: ");
-  scanf ("%d", pontosTuristicos1);
+  scanf ("%f", &pib1);
+  printf ("Digite a quantidade de pontos turisticos dessa cidade: ");
+  scanf ("%d", &pontosTuristicos1);
 
   // Dados da Carta 2
   printf ("\n\nInforme os dados da Carta 2\nEscolha de A a H para o nome do estado: ");
-  scanf (" %c", estado2);
-  printf ("Escolha entre A01,A04,A03,A04 para o código do estado: ");
+  scanf (" %c", &estado2);
+  printf ("Escolha entre 01,04,03,04 para o codigo do estado: ");
   scanf ("%s", codigo2);
   printf ("Digite o nome da cidade: ");
   scanf ("%s", cidade2);
   printf ("Digite a populacao dessa cidade: ");
-  scanf ("%d", populacao2);
+  scanf ("%d", &populacao2);
   printf ("Digite o tamanho em km² dessa cidade: ");
-  scanf ("%f", area2);
+  scanf ("%f", &area2);
   printf ("Digite o PIB dessa cidade: ");
-  scanf ("%f", pib2);
-  rintf ("Digite a quantidade de pontos turisticos dessa cidade: ");
-  scanf ("%d", pontosTuristicos2);
+  scanf ("%f", &pib2);
+  printf ("Digite a quantidade de pontos turisticos dessa cidade: ");
+  scanf("%d", &pontosTuristicos2);
 
   // Área para exibição dos dados da cidade
   // O uso do printf aqui é apenas para a exibição dos dados acima
   // Carta 1
-  printf ("Carta 1\nEstado: %c\nCodigo: %s\nNome da Cidade: %s\nPopulação: %d\nArea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d", estado1, codigo1, cidade1, populacao1, area1, pib1, pontosTuristicos1);
+  printf ("\nCarta 1\nEstado: %c\nCodigo: %c%s\nNome da Cidade: %s\nPopulacao: %d\nArea: %f km²\nPIB: %f bilhoes de reais\nNumero de Pontos Turisticos: %d\n", estado1, estado1, codigo1, cidade1, populacao1, area1, pib1, pontosTuristicos1);
   //Carta 2
-  printf ("Carta 1\nEstado: %c\nCodigo: %s\nNome da Cidade: %s\nPopulação: %d\nArea: %f km²\nPIB: %f bilhões de reais\nNúmero de Pontos Turísticos: %d", estado2, codigo2, cidade2, populacao2, area2, pib2, pontosTuristicos2);
+  printf ("\nCarta 2\nEstado: %c\nCodigo: %c%s\nNome da Cidade: %s\nPopulacao: %d\nArea: %f km²\nPIB: %f bilhoes de reais\nNumero de Pontos Turisticos: %d\n", estado2, estado2, codigo2, cidade2, populacao2, area2, pib2, pontosTuristicos2);
 
   //Fim
 return 0;
-} 
+}
